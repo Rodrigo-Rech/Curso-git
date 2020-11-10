8bb0827cb7857866cf0a3079c3594148701d77fd
Rodrigo-Rech <Rodrigol_rech@hotmail.com>
2020-11-10 11:11:18 -0300

    Projeto iniciado mostrando msg ola mundo

---
+++ Teste Git Hub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_Git_Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
