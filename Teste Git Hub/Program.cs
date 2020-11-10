bc874437a1d38f94b56b43fd85bb9688c24d17fe
Rodrigo-Rech <Rodrigol_rech@hotmail.com>
2020-11-10 11:12:57 -0300

    Adicionado msg de Bom dia

--- Teste Git Hub/Program.cs
+++ Teste Git Hub/Program.cs
@@ -7,6 +7,7 @@ namespace Teste_Git_Hub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning");
         }
     }
 }
